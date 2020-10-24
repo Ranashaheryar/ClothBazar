58022eef0463edd0c02419614ff5b3afaba8ffcc
Rana Sherry <ranasherry94@gmail.com>
2020-10-24 19:39:47 +0500

    Added Catagories and Products

---
+++ ClothBazar.Database/Migrations/202010241425015_intitalized.cs
@@ -0,0 +1,44 @@
+﻿namespace ClothBazar.Database.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class intitalized : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Catagories",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Decription = c.String(),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        Name = c.String(),
+                        Decription = c.String(),
+                        Catagory_ID = c.Int(),
+                    })
+                .PrimaryKey(t => t.ID)
+                .ForeignKey("dbo.Catagories", t => t.Catagory_ID)
+                .Index(t => t.Catagory_ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Products", "Catagory_ID", "dbo.Catagories");
+            DropIndex("dbo.Products", new[] { "Catagory_ID" });
+            DropTable("dbo.Products");
+            DropTable("dbo.Catagories");
+        }
+    }
+}
